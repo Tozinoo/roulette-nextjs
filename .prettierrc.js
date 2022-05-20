74b51386819355c0dfb8d9658dbb3f193abcb32b
CHOI HYUN SEOK <89627319+Tozinoo@users.noreply.github.com>
2022-05-20 16:04:01 +0900

    Feat: init nextJS

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    singleQuote: true, // 싱글쿼터 사용
+    semi: true, // 세미콜론 사용
+    useTabs: false, // 탭 사용 여부
+    tabWidth: 4, // 탭 너비
+    trailingComma: 'all', // 여러 줄 사용 시 후행 콤마
+    printWidth: 80, // 줄 바꿈 한폭 길이
+    arrowParens: 'avoid', // 화살표함수 괄호 표시
+};
